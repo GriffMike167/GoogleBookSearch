1c7b996a1d8409c2edefa00f09da4db77b7fac0b
Michael Griffith <griffithcmichael@gmail.com>
2021-01-26 00:40:58 -0600

    BookSchema

---
+++ models/book.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const bookSchema = new Schema ({
+    title: { type: [String], required: true };
+    title: { type: [String], required: true };
+    description: String;
+    href: String,
+    thumbnail: String, 
+})
+
+const Book = mongooose.model("Book", bookSchema)
+
+
+module.exports = Book;
